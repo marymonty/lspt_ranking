ba32fedec35e2c9abc74edf38cc4064400fc9aa2
chris-j-tang <chrisjtang98@gmail.com>
2019-12-03 11:19:20 -0500

    Added documentation for startup.py

--- src/startup.py
+++ src/startup.py
@@ -1,3 +1,5 @@
+"""Initializes and performs necessary startup operations for the Ranking team's server.
+"""
 import os
 import json
 import sys
@@ -6,10 +8,18 @@ from shutil import copyfile
 from .defines import POPULARITY, RECENCY, RELEVANCY, ENDPOINT_PATH, WEIGHT_PATH
 
 def startup():
+    """Initializes the server, paths within the server, and stable storage.
+    """
     create_storage()
     add_server_paths()
 
 def create_storage():
+    """Creates the directory, file, and default values for the weights.
+
+    Makes the /bin directory if it does not exist on the server, then creates
+    or opens the file used to store the weights. A JSON string composed of the
+    weights is written to this file.
+    """
     if not os.path.exists('lspt_ranking/bin'):
         os.mkdir('lspt_ranking/bin')
     if not os.path.exists('lspt_ranking/bin/weight_file.json'):
@@ -18,6 +28,13 @@ def create_storage():
             f.write(json.dumps(def_weights))
 
 def add_server_paths():
+    """Retrieves and populates the ENDPOINT_PATH constant.
+
+    Makes the /bin directory if it does not exist on the server. If the endpoints.json
+    file is missing on the server, an error is printed to STDERR. Otherwise, the
+    ENDPOINT_PATH constant from the defines module is populated by the contents of
+    the endpoints.json file.
+    """
     if not os.path.exists('lspt_ranking/bin'):
         os.mkdir('lspt_ranking/bin')
     if not os.path.exists('lspt_ranking/endpoints.json'):
