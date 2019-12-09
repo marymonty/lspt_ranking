27236a5b56b005698bbb54934a79e8152a01c909
Mary Montgomery <montgm2@rpi.edu>
2019-12-09 16:57:27 -0500

    commented calling.py

--- src/calling.py
+++ src/calling.py
@@ -1,3 +1,14 @@
+"""Module to create and check the Query object.
+Module description: 
+    A class for a Query, used to check that the given query can be made into
+    a json object and that falcon can successfully connect to the endpoint for
+    further computation.
+Typical usage example:
+    We receive query {query: "where in new york is rpi"}
+    this query can be successfully made into a json string
+    and even though an n_gram is not included, we can give 
+    a default 50 returned results.
+"""
 import json
 
 import falcon
@@ -9,13 +20,34 @@ CORRECT_REQUEST = "ERROR: Query paramater must be a string of length greater tha
 
 
 class Query(object):
-
+    """A class to represent a Query object.
+    A query is what is sent to us. We need to transform and perform computations
+    on this query, so we have made it into an object to be easier dealt with here.
+    We are checking to see if the Query is something that we can deal with.
+    The Query class has one function, on_post.
+    """
     def on_post(self, req, resp):
+        """Creates a json out of the query, if possible.
+        This function will put the given Query object through tests to 
+        see if if this input sent to us is in the proper format of what
+        we are asking for. If it is, we will convert the query into a json 
+        string and use it from there.
+        Args:
+            self: the Query object
+            req: the request, what querying sent to us
+            resp: the response
+        Raises:
+            EndpointException if fails to connect to endpoint
+            ServerErrorCode if connection to endpoint does not return 200 (success)
+        Returns:
+            none: unless exception is thrown, return to exit
+        """
         # Extract the query and fields from the request.
         bad_req = False
         try:
             to_rank = req.get_param('query', required=True)
             n_results = req.get_param('results', required=False)
+            #set the default number of results to be 50
             if n_results == None:
                 n_results = 50
         except ValueError:
