cce8f371c8ffdc0c39edd15dce73a67edb3555ed
Mary Montgomery <montgm2@rpi.edu>
2019-12-11 10:37:35 -0500

    documented app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""Starting falcon and adding our endpoint to the path.
+
+We call our startup function to start the server, set our api application,
+and add our endpoint route that is able to be called by other teams.
+"""
 import falcon
 
 from .src.calling import Query
